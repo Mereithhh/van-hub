f9ca9deede1c346bcbd7e2fe356e59e83b3f0547
mereith <wanglu@mereith.com>
2024-11-23 10:59:22 +0800

    feat: init db when start container

--- Dockerfile
+++ Dockerfile
@@ -37,18 +37,14 @@ ENV NODE_ENV=production
 # Uncomment the following line in case you want to disable telemetry during runtime.
 # ENV NEXT_TELEMETRY_DISABLED=1
 
-RUN addgroup --system --gid 1001 nodejs
-RUN adduser --system --uid 1001 nextjs
-
 COPY --from=builder /app/public ./public
 RUN mkdir -p data && mkdir -p public
 
 # Automatically leverage output traces to reduce image size
 # https://nextjs.org/docs/advanced-features/output-file-tracing
-COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
-COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
-
-USER nextjs
+COPY --from=builder  /app/.next/standalone ./
+COPY --from=builder  /app/.next/static ./.next/static
+COPY ./init-db.js ./
 
 EXPOSE 3000
 
@@ -58,4 +54,4 @@ ENV JWT_SECRET=van-hub-default-secret-key
 # server.js is created by next build from the standalone output
 # https://nextjs.org/docs/pages/api-reference/next-config-js/output
 ENV HOSTNAME="0.0.0.0"
-CMD ["node", "server.js"]
+CMD ["node", "init-db.js", "&&", "node", "server.js"]
---
+++ init-db.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const Database = require('better-sqlite3')
+const dbPath = path.join(process.cwd(), './data/data.db')
+const db = new Database(dbPath)
+
+// 创建必要的表和默认管理员用户
+db.exec(`
+  CREATE TABLE IF NOT EXISTS tools (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    url TEXT,
+    tags TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS settings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    siteTitle TEXT,
+    siteIcon TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
+
+  -- 检查是否已存在管理员用户，如果不存在则创建
+  INSERT OR IGNORE INTO users (username, password)
+  SELECT 'admin', '$2a$10$2bZFRQSxWW1uml2ZFfGcS.eVOInvKorFN6NYi6jxaQkAHE3JKRY16'  -- 默认密码：admin
+  WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
+`)
